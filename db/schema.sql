2a16d0b30fd01126a6f973799599a59f771e4051
RaduKostashchuk <kostasc@mail.ru>
2022-02-07 11:01:19 +0700

    0. Spring Security [#6879]

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,16 @@
+CREATE TABLE users (
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  enabled boolean default true,
+  PRIMARY KEY (username)
+);
+
+CREATE TABLE authorities (
+  username VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
+);
+
 CREATE TABLE IF NOT EXISTS type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50)
